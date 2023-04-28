a599dd2f66639209f1ff1d197440a31cc08cb20b
xHeXifx <100049779+xHeXifx@users.noreply.github.com>
2023-04-28 21:14:10 +0100

    Delete files directory

--- files/mover.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd %userprofile%\Desktop
-
-move main.bat %userprofile%\.zyron
-cd %userprofile%\.zyron
-start main.bat
-exit
--- files/reload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cd %userprofile%\Desktop
-start main.bat
